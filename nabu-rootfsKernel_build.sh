90096271ef60ae64b07eaf2980f9b1abf19f5d74
טפל בלוט <130299352+dpkg123@users.noreply.github.com>
2024-01-17 18:20:26 +0800

    Update nabu-rootfsKernel_build.sh

--- nabu-rootfsKernel_build.sh
+++ nabu-rootfsKernel_build.sh
@@ -36,7 +36,7 @@ mkfs.ext4 rootfs.img
 mkdir rootdir
 mount -o loop rootfs.img rootdir
 
-apt-get install debootstrap -y
+apt-get install debootstrap qemu-user-static -y
 debootstrap --arch=arm64 --include=w3m,ca-certificates,htop,neofetch,nethack-console,rmtfs,protection-domain-mapper,tqftpserv,bash-completion,fish,sudo,ssh --variant=minbase bookworm rootdir
 
 mount --bind /dev rootdir/dev
@@ -48,19 +48,6 @@ echo "nameserver 1.1.1.1" | tee rootdir/etc/resolv.conf
 echo "xiaomi-nabu" | tee rootdir/etc/hostname
 echo "127.0.0.1 localhost
 127.0.1.1 xiaomi-nabu" | tee rootdir/etc/hosts
-
-if uname -m | grep -q aarch64
-then
-  echo "cancel qemu install for arm64"
-else
-  wget https://github.com/multiarch/qemu-user-static/releases/download/v7.2.0-1/qemu-aarch64-static
-  install -m755 qemu-aarch64-static rootdir/
-
-  echo ':aarch64:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/qemu-aarch64-static:' | tee /proc/sys/fs/binfmt_misc/register
-  #ldconfig.real abi=linux type=dynamic
-  echo ':aarch64ld:M::\x7fELF\x02\x01\x01\x03\x00\x00\x00\x00\x00\x00\x00\x00\x03\x00\xb7:\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff:/qemu-aarch64-static:' | tee /proc/sys/fs/binfmt_misc/register
-fi
-
 
 #chroot installation
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:\$PATH
@@ -103,17 +90,6 @@ mkdir rootdir/var/lib/gdm
 touch rootdir/var/lib/gdm/run-initial-setup
 
 chroot rootdir apt clean
-
-if uname -m | grep -q aarch64
-then
-  echo "cancel qemu install for arm64"
-else
-  #Remove qemu emu
-  echo -1 | tee /proc/sys/fs/binfmt_misc/aarch64
-  echo -1 | tee /proc/sys/fs/binfmt_misc/aarch64ld
-  rm rootdir/qemu-aarch64-static
-  rm qemu-aarch64-static
-fi
 
 umount rootdir/sys
 umount rootdir/proc
